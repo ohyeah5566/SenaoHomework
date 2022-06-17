f454fe84b3cb1158ad19cdf25325784d6ac0540e
ohyeah5566 <ohyeah5566@gmail.com>
2022-06-17 14:32:50 +0800

    recyclerView adapter

--- app/src/main/java/com/ohyeah5566/senaohw/MainActivity.kt
+++ app/src/main/java/com/ohyeah5566/senaohw/MainActivity.kt
@@ -2,14 +2,53 @@ package com.ohyeah5566.senaohw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.ohyeah5566.senaohw.databinding.ActivityMainBinding
+import com.ohyeah5566.senaohw.databinding.ItemMartBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.recyclerView.adapter = MartAdapter(emptyList())
     }
+}
+
+class MartAdapter(
+    private val list: List<Mart>
+) : RecyclerView.Adapter<MartAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemMartBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ViewHolder(
+        val binding: ItemMartBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(mart: Mart) {
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/ohyeah5566/senaohw/Mart.kt
+++ app/src/main/java/com/ohyeah5566/senaohw/Mart.kt
@@ -9,16 +9,3 @@ data class Mart(
     val imageUrl: String = "",
     val stockAvailable: Int = 0
 )
-
-
-/*
-{
-            "price": 39950,
-            "martShortName": "iPhone 12 Pro Max 256GB",
-            "imageUrl": "https://pdinfo.senao.com.tw/octopus/contents/99b404a6bcfb4a74a27e4a10746fb258.jpg",
-            "finalPrice": 39950,
-            "martName": "iPhone 12 Pro Max 256GB【下殺97折 送保護貼兌換券】",
-            "stockAvailable": 30,
-            "martId": 1250797
-        }
- */
